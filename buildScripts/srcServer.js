2a1b551838cfd5c897efc1464b6c11711898be8d
blade-demon <949659359@qq.com>
2017-12-14 19:10:04 +0800

    add api centerized and async await support

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -22,6 +22,14 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
+app.get('/users', function(req, res) {
+    res.json([
+        { "id": 1, "firstName": "Bob", "lastName": "Smith", "email": "bob@gmail.com" },
+        { "id": 2, "firstName": "Tammy", "lastName": "Norton", "email": "tnorton@yahoo.com" },
+        { "id": 3, "firstName": "Tina", "lastName": "Lee", "email": "lee.tina@hotmail.com" }
+    ]);
+});
+
 app.listen(port, function(err) {
     if (err) {
         console.log(err);
--- src/index.js
+++ src/index.js
@@ -1,5 +1,54 @@
-import './index.css';
-import numeral from 'numeral';
+// import './index.css';
+
+// import { getUsers, deleteUser } from './api/userApi';
+
+// // Populate table of users via API call.
+// getUsers().then(result => {
+//     let usersBody = "";
+
+//     result.forEach(user => {
+//         usersBody += `<tr>
+//       <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
+//       <td>${user.id}</td>
+//       <td>${user.firstName}</td>
+//       <td>${user.lastName}</td>
+//       <td>${user.email}</td>
+//       </tr>`
+//     });
+
+//     global.document.getElementById('users').innerHTML = usersBody;
+
+//     const deleteLinks = global.document.getElementsByClassName('deleteUser');
+
+//     // Must use array.from to create a real array from a DOM collection
+//     // getElementsByClassname only returns an "array like" object
+//     Array.from(deleteLinks, link => {
+//         link.onclick = function(event) {
+//             const element = event.target;
+//             event.preventDefault();
+//             deleteUser(element.attributes["data-id"].value);
+//             const row = element.parentNode.parentNode;
+//             row.parentNode.removeChild(row);
+//         };
+//     });
+// });
+
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+var sleep = function(time) {
+    return new Promise(function(resolve, reject) {
+        setTimeout(function() {
+            resolve();
+        }, time);
+    })
+};
+
 /* eslint-disable no-console */
-const courseValue = numeral(100000).format('$0,0.00');
-console.log(`I would pay ${courseValue} for this awesome course!`);
+var start = async function() {
+    // 在这里使用起来就像同步代码那样直观
+    console.log('start');
+    await sleep(3000);
+    console.log('end');
+};
+
+start();
